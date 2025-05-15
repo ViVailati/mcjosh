71024bb6da71b7986d8e7fbcf650d224ad497ff9
Vi Vailati <vi.vailati.845@gmail.com>
2025-05-15 15:19:37 -0600

    Update root command to use workflow engine

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	client "github.com/ViVailati/mcjosh/http_client"
-	"github.com/ViVailati/mcjosh/models"
-	"github.com/manifoldco/promptui"
+	"github.com/ViVailati/mcjosh/workflow"
 	"github.com/spf13/cobra"
 )
 
@@ -15,28 +13,9 @@ var rootCmd = &cobra.Command{
 	Short: "Minecraft Java (o) Server Helper",
 	Long:  `McJosh is a command-line tool for managing Minecraft Java servers.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		prompt := promptui.Select{
-			Label: "What's the server type?",
-			Items: models.ServerTypes,
-			Templates: &promptui.SelectTemplates{
-				Active:   `▸ {{ .Name | cyan }}`,
-				Inactive: `{{ .Name | faint }}`,
-			},
-		}
-
-		i, _, err := prompt.Run()
-		if err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-
-		st := models.ServerTypes[i]
-
-		fmt.Printf("We're creating a %s server from: %s\n", st.Name, st.URL)
-
-		client := client.New()
-		if err := client.GetMinecraftVersion(); err != nil {
-			fmt.Println("Error:", err)
+		w := workflow.DefaultWorkflow()
+		if err := w.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, "Error:", err)
 			os.Exit(1)
 		}
 	},
