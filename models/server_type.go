8656f70d0a78d3b7c34c30329bd78aa6f41ba858
Vi Vailati <vi.vailati.845@gmail.com>
2025-05-15 15:20:01 -0600

    Remove additional server types for now

--- models/server_type.go
+++ models/server_type.go
@@ -9,7 +9,4 @@ type serverTypes []ServerType
 
 var ServerTypes = serverTypes{
 	{Name: "Vanilla", URL: "https://piston-data.mojang.com/v2/objects/"},
-	{Name: "Paper", URL: "https://papermc.io/api/v2/projects/paper"},
-	{Name: "Spigot", URL: "https://hub.spigotmc.org/nexus/content/repositories/snapshots/org/spigotmc/spigot/"},
-	{Name: "Purpur", URL: "https://purpur.pl3x.net/api/v2/"},
 }
