abd087d18b650ac017aa523318c0ccd26885dac2
Vi Vailati <vi.vailati.845@gmail.com>
2025-05-18 15:27:50 -0600

    Add GetHttpClient method to the context struct

--- models/context.go
+++ models/context.go
@@ -1,5 +1,17 @@
 package models
 
+import (
+	client "github.com/ViVailati/mcjosh/http_client"
+)
+
+const (
+	HttpClient = "http_client"
+)
+
 type Context struct {
 	Data map[string]any
+}
+
+func (c *Context) GetHttpClient() *client.Client {
+	return c.Data[HttpClient].(*client.Client)
 }
