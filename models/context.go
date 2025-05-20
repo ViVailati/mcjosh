473e6295c7f5a0205f26a156fd384a9397b5f988
Vi Vailati <vi.vailati.845@gmail.com>
2025-05-19 22:36:12 -0600

    Update the Context struct to explicitly add the Client

--- models/context.go
+++ models/context.go
@@ -4,14 +4,6 @@ import (
 	client "github.com/ViVailati/mcjosh/http_client"
 )
 
-const (
-	HttpClient = "http_client"
-)
-
 type Context struct {
-	Data map[string]any
-}
-
-func (c *Context) GetHttpClient() *client.Client {
-	return c.Data[HttpClient].(*client.Client)
+	Client *client.Client
 }
